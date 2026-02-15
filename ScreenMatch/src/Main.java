bb7afad476d81f540bac1c2cd0c23f6db292a4e7
Nathan Quintao <nathanquintaounip@gmail.com>
2026-02-15 10:26:43 -0300

    Início de projeto - Alura

---
+++ ScreenMatch/src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Esse é o Screen Match" + "\n");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int ano = 2022;
+
+        System.out.println("Ano de lançamento: " + ano);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+    }
+}
