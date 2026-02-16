0dc2e07c5644dd9e70b941ebf07afa6231935a71
Nathan Quintao <nathanquintaounip@gmail.com>
2026-02-16 12:00:16 -0300

    Adicionando classes e realizando testes de programação

--- ScreenMatch/src/Main.java
+++ ScreenMatch/src/Main.java
@@ -8,9 +8,19 @@ public class Main {
 
         int ano = 2022;
 
+
         System.out.println("Ano de lançamento: " + ano);
-        boolean incluidoNoPlano = true;
-        double notaDoFilme = 8.1;
+
+        double media = (9.8 + 6.3 + 8.0) /3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = """
+        Filme de aventura com galã dos anos 80
+        """;
+        System.out.println(sinopse);
+
+        int classificacao = (int) (media /2);
+        System.out.println(classificacao);
 
     }
 }
